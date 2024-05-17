b11eb019553bb64a2c7bb707f41df991585edae3
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-17 13:32:27 +0900

    fix: wrong argument type for insert podcaster query

--- authentication/utils.py
+++ authentication/utils.py
@@ -10,7 +10,7 @@ def create_unverified(*attr: tuple[str]) -> bool:
     with connection.cursor() as cursor:
         try:
             cursor.execute(
-                "INSERT INTO akun (email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal)"
+                "INSERT INTO akun (email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal) "
                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                 attr
             )
@@ -26,7 +26,7 @@ def create_verified(podcaster: str, artist: str, songwriter: str, *attr: tuple[s
     with connection.cursor() as cursor:
         try:
             cursor.execute(
-                "INSERT INTO akun (email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal)"
+                "INSERT INTO akun (email, password, nama, gender, tempat_lahir, tanggal_lahir, is_verified, kota_asal) "
                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                 attr
             )
@@ -34,13 +34,13 @@ def create_verified(podcaster: str, artist: str, songwriter: str, *attr: tuple[s
             return False
         
         if podcaster:
-            cursor.execute("INSERT INTO podcaster (email) VALUES (%s)", attr[0])
+            cursor.execute("INSERT INTO podcaster (email) VALUES (%s)", [attr[0]])
         
         if pemilik_hak_cipta:
             pemilik_hak_cipta_id = uuid4()
             rate_royalti = randint(1, 10)
             cursor.execute(
-                "INSERT INTO pemilik_hak_cipta (id, rate_royalti)"
+                "INSERT INTO pemilik_hak_cipta (id, rate_royalti) "
                 "VALUES (%s, %s)",
                 (pemilik_hak_cipta_id, rate_royalti)
             )
@@ -48,14 +48,14 @@ def create_verified(podcaster: str, artist: str, songwriter: str, *attr: tuple[s
             if artist:
                 artist_id = uuid4()
                 cursor.execute(
-                    "INSERT INTO artist (id, email_akun, id_pemilik_hak_cipta)"
+                    "INSERT INTO artist (id, email_akun, id_pemilik_hak_cipta) "
                     "VALUES (%s, %s, %s)",
                     (artist_id, attr[0], pemilik_hak_cipta_id)
                 )
                 
             if songwriter:
                 cursor.execute(
-                    "INSERT INTO songwriter (id, email_akun, id_pemilik_hak_cipta)"
+                    "INSERT INTO songwriter (id, email_akun, id_pemilik_hak_cipta) "
                     "VALUES (%s, %s, %s)",
                     (artist_id, attr[0], pemilik_hak_cipta_id)
                 )
@@ -69,7 +69,7 @@ def create_label(*attr: tuple[str]) -> bool:
         pemilik_hak_cipta_id = uuid4()
         rate_royalti = randint(1, 10)
         cursor.execute(
-            "INSERT INTO pemilik_hak_cipta (id, rate_royalti)"
+            "INSERT INTO pemilik_hak_cipta (id, rate_royalti) "
             "VALUES (%s, %s)",
             (pemilik_hak_cipta_id, rate_royalti)
         )
@@ -77,7 +77,7 @@ def create_label(*attr: tuple[str]) -> bool:
         label_id = uuid4()
         try:
             cursor.execute(
-                "INSERT INTO label (id, email, password, nama, kontak, id_pemilik_hak_cipta)"
+                "INSERT INTO label (id, email, password, nama, kontak, id_pemilik_hak_cipta) "
                 "VALUES (%s, %s, %s, %s, %s, %s)",
                 (label_id, *attr, pemilik_hak_cipta_id)
             )

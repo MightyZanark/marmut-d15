4d9e0691b2920710692a95592b791a791da503a5
vinamyrnauli <vinasllgn@gmail.com>
2024-05-19 00:41:58 +0700

    add: label to login

--- authentication/views.py
+++ authentication/views.py
@@ -100,8 +100,12 @@ def login(request):
             cursor.execute("SELECT * FROM akun WHERE email = %s AND password = %s", [email, password])
             user = cursor.fetchone()
 
+            if user is None:
+                cursor.execute("SELECT * FROM label WHERE email = %s AND password = %s", [email, password])
+                user = cursor.fetchone()
+
             if user is not None:
-                request.session['user_email'] = user[0]
+                request.session['user_email'] = str(user[0])
                 request.session['user_type'] = determine_user_type(email, cursor)
                 return redirect('main:dashboard')
             else:
